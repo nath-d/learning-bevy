6049612aba47bb05334743c00a44180229e0fd71
nath-d <nath.deborshi0711@gmail.com>
2026-03-29 04:11:51 +0530

    first commit

---
+++ src/main.rs
@@ -0,0 +1,64 @@
+use bevy::prelude::*;
+
+mod player;
+
+use player::PlayerPlugin;
+
+fn main() {
+    App::new()
+    .add_plugins(DefaultPlugins)
+    .add_systems(Startup, game_setup)
+    .add_systems(Update, move_player)
+    .run();
+
+}
+
+fn game_setup(mut commands: Commands) {
+    commands.spawn(Camera2d);
+
+    commands.spawn((
+        Text2d::new("@"),
+        TextFont {
+            font_size: 12.0,
+            font: default(),
+            ..default()
+        },
+        TextColor(Color::srgba(0.0, 1.0, 0.0, 1.0)), //(r, g, b, alpha)
+        Transform::from_translation(Vec3::ZERO),
+        Player,
+        ));
+}
+
+#[derive(Component)]
+struct Player;
+
+fn move_player(
+    input: Res<ButtonInput<KeyCode>>, 
+    time: Res<Time>,
+    mut player_transform: Single<&mut Transform, With<Player>>,
+    ) {
+
+    let mut direction = Vec2::ZERO;
+
+    if input.pressed(KeyCode::KeyA) {
+        direction.x -= 1.0;
+    }
+    if input.pressed(KeyCode::KeyD) {
+        direction.x += 1.0;
+    }
+    if input.pressed(KeyCode::KeyW) {
+        direction.y += 1.0;
+    }
+    if input.pressed(KeyCode::KeyS) {
+        direction.y -= 1.0;
+    }
+
+    if direction != Vec2::ZERO {
+        let speed = 300.0; //pixels per second
+        let delta = direction.normalize() * speed * time.delta_secs(); 
+        //because direction is a vector, multiplication produces vector
+        
+        player_transform.translation.x += delta.x;
+        player_transform.translation.y += delta.y;
+    }
+}
